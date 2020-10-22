5f2ce61578fc584f219a34eb342908750d178f88
Ren YAMAMOTO <rennnn0806@gmail.com>
2020-10-22 09:31:10 +0900

    message受取修正

--- src/Components/Main/Chat.tsx
+++ src/Components/Main/Chat.tsx
@@ -161,7 +161,7 @@ const Chat: FC<Props> = ({cableApp, roomsGet}) =>{
       { withCredentials: true}
       ).then(response => {
         if (response.data.status === 'created'){
-
+          setMessage(response.data.message)
         }
       }).catch(error => {
         console.log("message error", error)
